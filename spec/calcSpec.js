373c3dd7468ab0e6c27cb692641afbc5ac151827
marlene32100 <katia.debon@hotmail.it>
2020-08-19 10:03:46 +0000

    Added addition test

---
+++ spec/calcSpec.js
@@ -0,0 +1,8 @@
+describe("Calculator", function() {
+    describe("Addition function", function() {
+        it("should return 42", function(){
+            expect(addition(20, 22)).toBe(42);
+        });
+
+    });
+});
